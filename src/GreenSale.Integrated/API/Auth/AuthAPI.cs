1f2791f7769d632485114aba26fd84c4b65f16f8
SamandarYigitaliyev <programmist4467@gmail.com>
2023-08-22 11:20:00 +0500

    connected to the login page api

---
+++ src/GreenSale.Integrated/API/Auth/AuthAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenSale.Integrated.API.Auth
+{
+    public class AuthAPI
+    {
+        public static readonly string BASE_URL = "http://95.130.227.68:8080";
+        public static readonly string LoginApi = BASE_URL + "/api/auth/login";
+
+    }
+}
