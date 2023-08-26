0d2dd03e35e1e4c5e46f28a8819d71f124f2f048
AbdurahmonJumanboyev <abdurahmonnt2003@gmail.com>
2023-08-27 01:38:33 +0500

    fixed

--- src/GreenSale.Desktop/Windows/Products/StorageProductFullViewWindow.xaml.cs
+++ src/GreenSale.Desktop/Windows/Products/StorageProductFullViewWindow.xaml.cs
@@ -70,7 +70,17 @@ namespace GreenSale.Desktop.Windows.Products
 
         private void btnPicture_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            if (btnPicture.IsMouseOver == true)
+            {
+                ImgIcon.Visibility = Visibility.Visible;
+                btnPicture.BorderThickness = new Thickness(2);
 
+            }
+            else
+            {
+                ImgIcon.Visibility = Visibility.Hidden;
+                ImgUpdateMain.BorderThickness = new Thickness(0);
+            }
         }
     }
 }
