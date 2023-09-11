6f3d30278f39383d5f7ec56a1dc69fbf11a64697
rustambekovich <muhammadqodir5050@gmail.com>
2023-09-11 23:01:04 +0500

    ForgotPassword Disigen fineshed

--- src/GreenSale.Desktop/Windows/Auth/ForgotPasswordWindow.xaml.cs
+++ src/GreenSale.Desktop/Windows/Auth/ForgotPasswordWindow.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using static GreenSale.Integrated.Services.UseWindowServices.BlurBehindDemo;
 
 namespace GreenSale.Desktop.Windows.Auth
 {
+
+
+
     /// <summary>
     /// Interaction logic for ForgotPasswordWindow.xaml
     /// </summary>
@@ -38,5 +34,41 @@ namespace GreenSale.Desktop.Windows.Auth
         {
             this.Close();
         }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+        internal void EnableBlur()
+        {
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            EnableBlur();
+        }
+
+        private void NumberValidationTextBox(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
     }
 }
+
