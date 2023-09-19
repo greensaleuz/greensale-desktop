bf17140f52363d93821a05f3e8a10b4da9169d35
rustambekovich <muhammadqodir5050@gmail.com>
2023-09-20 03:03:20 +0500

    CODE RUB : Change Base API

--- src/GreenSale.ViewModels/Models/SellerPosts/SellerPost.cs
+++ src/GreenSale.ViewModels/Models/SellerPosts/SellerPost.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GreenSale.ViewModels.Models.SellerPosts;
 
-namespace GreenSale.ViewModels.Models.SellerPosts
+public class SellerPost
 {
-    public class SellerPost
-    {
-        public long Id { get; set; }
-        public string region { get; set; } = string.Empty;
-        public string title { get; set; } = string.Empty;
-        public string description { get; set; } = string.Empty;
-        public double price { get; set; }
-        public int capacity { get; set; }
-        public string capacityMeasure { get; set; } = string.Empty;
-        public string type { get; set; } = string.Empty;
-        public DateTime updatedAt { get; set; }
-        public string  status { get; set; } = string.Empty;
-        public string mainImage { get; set; } = string.Empty; 
-    }
+    public long Id { get; set; }
+    public string region { get; set; } = string.Empty;
+    public string title { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
+    public double price { get; set; }
+    public int capacity { get; set; }
+    public string capacityMeasure { get; set; } = string.Empty;
+    public string type { get; set; } = string.Empty;
+    public DateTime updatedAt { get; set; }
+    public string status { get; set; } = string.Empty;
+    public string mainImage { get; set; } = string.Empty;
+    public double AverageStars { get; set; }
 }
