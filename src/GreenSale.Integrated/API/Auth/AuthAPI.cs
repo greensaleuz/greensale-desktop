1da928708b11b10a56d6221360b56e4f43de5903
rustambekovich <muhammadqodir5050@gmail.com>
2023-09-21 21:43:04 +0500

    Login Windeow Validatoer Created

--- src/GreenSale.Integrated/API/Auth/AuthAPI.cs
+++ src/GreenSale.Integrated/API/Auth/AuthAPI.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GreenSale.Integrated.API.Auth;
 
-namespace GreenSale.Integrated.API.Auth
+public class AuthAPI
 {
-    public class AuthAPI
-    {
-        public static readonly string BASE_URL = "http://139.59.96.168:89";
-        public static readonly string BASE_URL_IMG = "http://139.59.96.168:89/";
-        public static readonly string LoginApi = BASE_URL + "/api/auth/login";
+    public static readonly string BASE_URL = "http://139.59.96.168:89";
+    public static readonly string BASE_URL_IMG = "http://139.59.96.168:89/";
+    public static readonly string LoginApi = BASE_URL + "/api/auth/login";
 
-    }
 }
